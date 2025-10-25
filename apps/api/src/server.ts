b67bc73198f48e2e674429472849e1bbea8a8ae5
Michael Kellar <michael.d.kellar@gmail.com>
2025-10-24 19:32:03 -0700

    fix cors issue

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -75,7 +75,7 @@ const limiter = rateLimit({
 
 export const app = express();
 app.use(cors({
-    origin: process.env.WEB_ORIGIN || "http://localhost:3000",
+    origin: process.env.WEB_ORIGIN || "http://localhost:5173",
     methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type","Authorization"],
     credentials: true,  // if you allow cookies/auth headers
